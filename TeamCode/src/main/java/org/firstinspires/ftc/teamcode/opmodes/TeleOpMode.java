b7688a3a0ffdf751de704b3c5c3ac32c4480f759
Devildogs11206 <devildogs11206@gmail.com>
2020-09-30 17:12:52 -0500

    deleted unneeded controller code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOpMode.java
@@ -2,16 +2,10 @@ package org.firstinspires.ftc.teamcode.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.teamcode.controllers.AssistController;
-import org.firstinspires.ftc.teamcode.controllers.ClawController;
 import org.firstinspires.ftc.teamcode.controllers.DriveController;
-import org.firstinspires.ftc.teamcode.controllers.LiftController;
 import org.firstinspires.ftc.teamcode.controllers.LightsController;
 import org.firstinspires.ftc.teamcode.controllers.RecorderController;
 import org.firstinspires.ftc.teamcode.controllers.RobotController;
-import org.firstinspires.ftc.teamcode.controllers.SlideController;
-import org.firstinspires.ftc.teamcode.controllers.StickController;
-import org.firstinspires.ftc.teamcode.controllers.TiltController;
 import org.firstinspires.ftc.teamcode.internal.Alliance;
 
 import static org.firstinspires.ftc.teamcode.internal.Alliance.UNKNOWN;
