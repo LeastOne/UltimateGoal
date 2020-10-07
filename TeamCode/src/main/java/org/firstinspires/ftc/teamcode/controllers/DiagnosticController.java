baa0259fb17400250c3abccbd17dd05ef250d934
Devildogs11206 <devildogs11206@gmail.com>
2020-10-07 16:54:34 -0500

    created diagnostic controller with start buttons

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/DiagnosticController.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.controllers;
+
+import org.firstinspires.ftc.teamcode.opmodes.OpMode;
+
+public class DiagnosticController extends RobotController {
+    public DiagnosticController(OpMode opMode) { super(opMode); }
+
+    @Override
+    public void execute() {
+        if (gamepad1.back && gamepad1.start && gamepad1.atRest()) {
+
+
+        }
+    }
+}
