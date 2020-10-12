795feb4bdcee5dfbb6f62bd4c7783326d3bad416
Devildogs11206 <devildogs11206@gmail.com>
2020-10-12 15:50:07 -0500

    Merged DriveController with MecanumController (deleted MecanumController)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/DriveController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/DriveController.java
@@ -11,13 +11,36 @@ public class DriveController extends RobotController {
 
     @Override
     public void execute() {
-        if(!robot.mecanumMode){
-            double drive = -gamepad1.left_stick_y;
-            double turn = gamepad1.right_stick_x;
-            robot.drive(drive * POWER,0, turn * POWER,0);
+        if(robot.mecanumMode){
+            driveMecanum();
+        } else{
+            driveStandard();
         }
-        else{
-            if(gamepad1.left_stick_button&&gamepad1.right_stick_button){robot.mecanumMode=false;}
-        }
+    }
+
+    private void driveStandard() {
+        double drive = -gamepad1.left_stick_y;
+        double turn = gamepad1.right_stick_x;
+        robot.drive(drive * POWER,0, turn * POWER,0);
+    }
+
+    private void driveMecanum() {
+        //since left stick can be pushed in all directions to control the robot's movements, its "power" must be the actual
+        //distance from the center, or the hypotenuse of the right triangle formed by left_stick_x and left_stick_y
+        double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
+
+        //angle between x axis and "coordinates" of left stick
+        double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
+
+        //turn
+        double rightX = gamepad1.right_stick_x;
+
+        double lf = r * Math.cos(robotAngle) + rightX;;
+        double lb = r * Math.sin(robotAngle) + rightX;;
+        double rf = r * Math.sin(robotAngle) - rightX;;
+        double rb = r * Math.cos(robotAngle) - rightX;;
+
+        robot.drive(lf,lb,rf,rb);
+
     }
 }
