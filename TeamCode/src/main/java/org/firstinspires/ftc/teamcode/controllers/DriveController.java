16d6eecd44bcaca31a97a0d6a0b96d33df5b2117
Devildogs11206 <devildogs11206@gmail.com>
2020-10-14 15:51:21 -0500

    some formatting changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/DriveController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/DriveController.java
@@ -11,7 +11,7 @@ public class DriveController extends RobotController {
 
     @Override
     public void execute() {
-        if(robot.mecanumMode){
+        if(robot.mecanumMode) {
             driveMecanum();
         } else{
             driveStandard();
@@ -22,8 +22,8 @@ public class DriveController extends RobotController {
         double drive = -gamepad1.left_stick_y;
         double turn = gamepad1.right_stick_x;
 
-        double left = POWER*(drive+turn);
-        double right = POWER*(drive-turn);
+        double left = POWER * (drive+turn);
+        double right = POWER * (drive-turn);
 
         double max = Math.max(Math.abs(left), Math.abs(right));
 
@@ -33,26 +33,24 @@ public class DriveController extends RobotController {
         }
 
         robot.drive(left, left, right, right);
-
     }
 
     private void driveMecanum() {
         //since left stick can be pushed in all directions to control the robot's movements, its "power" must be the actual
         //distance from the center, or the hypotenuse of the right triangle formed by left_stick_x and left_stick_y
-        double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
+        double r = Math.hypot(gamepad1.left_stick_x, -gamepad1.left_stick_y);
 
         //angle between x axis and "coordinates" of left stick
-        double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
+        double robotAngle = Math.atan2(-gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
 
         //turn
         double rightX = gamepad1.right_stick_x;
 
-        double lf = r * Math.cos(robotAngle) + rightX;;
-        double lb = r * Math.sin(robotAngle) + rightX;;
-        double rf = r * Math.sin(robotAngle) - rightX;;
-        double rb = r * Math.cos(robotAngle) - rightX;;
+        double lf = POWER * (r * Math.cos(robotAngle) + rightX);
+        double lr = POWER * (r * Math.sin(robotAngle) + rightX);
+        double rf = POWER * (r * Math.sin(robotAngle) - rightX);
+        double rr = POWER * (r * Math.cos(robotAngle) - rightX);
 
-        robot.drive(lf,lb,rf,rb);
-
+        robot.drive(lf, lr, rf, rr);
     }
 }
