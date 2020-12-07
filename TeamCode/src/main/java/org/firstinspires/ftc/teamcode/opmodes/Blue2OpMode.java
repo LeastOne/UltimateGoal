b790ba0bb87d3e86a51c9ddd341f92d027bc4353
Devildogs11206 <devildogs11206@gmail.com>
2020-12-07 16:48:36 -0600

    added target a to Blue2OpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Blue2OpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Blue2OpMode.java
@@ -6,6 +6,16 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 public class Blue2OpMode extends BlueOpMode {
     @Override
     protected void execute() {
-
+        this.targetA();
     }
+
+    protected void targetA() {
+        robot.drive(1,0,0,34);
+        robot.drive(1,0,0,24);
+        robot.drive(1,0,90,48);
+        robot.drive(1,0,180,72);
+        robot.drive(1,0,-100,7);
+        robot.drive(1,0,-30,60);
+    }
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/BlueAOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/BlueAOpMode.java
@@ -6,8 +6,8 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 public class BlueAOpMode extends BlueOpMode {
     @Override
     protected void execute() {
-        robot.drive(1,0,0,24);
         robot.drive(1,0,0,34);
+        robot.drive(1,0,0,24);
         robot.drive(1,0,90,48);
         robot.drive(1,0,180,72);
         robot.drive(1,0,-100,7);
