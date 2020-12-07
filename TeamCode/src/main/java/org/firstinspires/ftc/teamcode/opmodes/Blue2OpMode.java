455024ead61bd21968e239957bff5912c752e06e
Devildogs11206 <devildogs11206@gmail.com>
2020-12-07 16:50:35 -0600

    added target B to blue2opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Blue2OpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Blue2OpMode.java
@@ -7,6 +7,7 @@ public class Blue2OpMode extends BlueOpMode {
     @Override
     protected void execute() {
         this.targetA();
+        this.targetB();
     }
 
     protected void targetA() {
@@ -16,6 +17,16 @@ public class Blue2OpMode extends BlueOpMode {
         robot.drive(1,0,180,72);
         robot.drive(1,0,-100,7);
         robot.drive(1,0,-30,60);
+    }
+
+    protected void targetB() {
+        robot.drive(1,0,0,24);
+        robot.drive(1,0,20,48);
+        robot.drive(1,0,-160,12);
+        robot.drive(1,0,160,54);
+        robot.drive(1,0,-90,24);
+        robot.drive(1,0,-7,72);
+
     }
 
 }
