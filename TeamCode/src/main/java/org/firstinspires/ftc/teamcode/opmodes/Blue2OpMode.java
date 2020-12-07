9600d408e6bb4d0a9296d38976946da105853480
Devildogs11206 <devildogs11206@gmail.com>
2020-12-07 16:51:49 -0600

    Added targetC method to Blue2OpMode.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Blue2OpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Blue2OpMode.java
@@ -8,6 +8,7 @@ public class Blue2OpMode extends BlueOpMode {
     protected void execute() {
         this.targetA();
         this.targetB();
+        this.targetC();
     }
 
     protected void targetA() {
@@ -27,6 +28,23 @@ public class Blue2OpMode extends BlueOpMode {
         robot.drive(1,0,-90,24);
         robot.drive(1,0,-7,72);
 
+    }
+
+    protected void targetC() {
+        robot.drive(1,0,0,36); //drive up to rings
+        //scan for ring amount
+        robot.drive(1,0,0,78); //drive to back of target area (but leave room for wobble goal)
+        //deposit first wobble goal
+        robot.drive(-1,0,0,24);//back up
+        robot.drive(0,1,0,48);//strafe right
+        robot.drive(0,-1,-90,66);//strafe to line up with second wobble goal
+        robot.drive(1,0,-90,30);//push wobble goal into position of original wobble goal
+        robot.drive(-1,0,-90,6);//back up slightly
+        robot.drive(-1,0,0,13);//back up to wall
+        robot.drive(0,-1,0,15);//strafe left
+        robot.drive(1,0,0,107);//push second wobble goal into target area
+        //deposit second wobble goal
+        robot.drive(-1,0,0,36);//back up to line
     }
 
 }
