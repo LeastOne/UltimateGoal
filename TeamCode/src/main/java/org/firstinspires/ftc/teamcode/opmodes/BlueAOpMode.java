0827259774d9ad63e84b143f1ffad27fa7219999
Devildogs11206 <devildogs11206@gmail.com>
2020-12-07 16:20:44 -0600

    added Blue a op mode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/BlueAOpMode.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous
+public class BlueAOpMode extends BlueOpMode {
+    @Override
+    protected void execute() {
+        robot.drive(1,0,0,24);
+        robot.drive(1,0,0,34);
+        robot.drive(1,0,90,48);
+        robot.drive(1,0,180,72);
+        robot.drive(1,0,-100,7);
+        robot.drive(1,0,-30,60);
+    }
+}
