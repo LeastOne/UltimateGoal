bcb98f7870fe1874b8a0e66586f916595d4bfccd
Devildogs11206 <devildogs11206@gmail.com>
2020-12-30 09:30:21 -0600

    changed turbo mode to 'mollasses turtle' mode (name still pending

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/DriveController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/DriveController.java
@@ -10,7 +10,7 @@ public class DriveController extends RobotController {
     @Override
     public void execute() {
         if (gamepad1.dpad_up) robot.drivePower = 1;
-        else if (gamepad1.dpad_down) robot.drivePower = .5;
+        else if (gamepad1.dpad_down) robot.drivePower = .2;
         robot.drive(
            -gamepad1.left_stick_y,
             gamepad1.left_stick_x,
