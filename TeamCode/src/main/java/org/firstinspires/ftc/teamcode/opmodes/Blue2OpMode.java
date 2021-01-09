e37dad1871d4a47bd36c8c9fe149b51e53af35ea
Devildogs11206 <devildogs11206@gmail.com>
2021-01-09 10:50:11 -0600

    changed wills code to more from 7 to 9

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Blue2OpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Blue2OpMode.java
@@ -43,7 +43,7 @@ public class Blue2OpMode extends BlueOpMode {
         robot.drive(-1,0,0,40);
         robot.drive(0,1,0,32);
         robot.drive(1,0,0,42);
-        robot.drive(1,0,7,36);
+        robot.drive(1,0,9,36);
     }
 
     protected void targetC() {
