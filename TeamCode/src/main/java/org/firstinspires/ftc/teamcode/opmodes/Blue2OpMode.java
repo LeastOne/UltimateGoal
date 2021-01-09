70dd7f19a4a272073582ba719fc675264a1a2dfe
Devildogs11206 <devildogs11206@gmail.com>
2021-01-09 10:48:46 -0600

    added backup to zoes code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Blue2OpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Blue2OpMode.java
@@ -53,6 +53,7 @@ public class Blue2OpMode extends BlueOpMode {
         robot.drive(0,1,0,32);
         robot.drive(1,0,0,42);
         robot.drive(1,0,20,70);
+        robot.drive(-1,0,0,30);
     }
     //Back up
 }
