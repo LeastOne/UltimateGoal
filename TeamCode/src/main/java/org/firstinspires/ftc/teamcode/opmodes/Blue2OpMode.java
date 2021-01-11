e4e466bbeaeb92339f7fd1de597e68c48b20afc0
Devildogs11206 <devildogs11206@gmail.com>
2021-01-11 16:12:23 -0600

    added backup and strafe

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Blue2OpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Blue2OpMode.java
@@ -28,6 +28,8 @@ public class Blue2OpMode extends BlueOpMode {
         robot.drive(-1,0,0,66);
         robot.drive(0,1,0,36);
         robot.drive(1,0,30,64);
+        robot.drive(-1,0,30,5);
+        robot.drive(0,1,30,18);
     }
 
     protected void targetB() {
