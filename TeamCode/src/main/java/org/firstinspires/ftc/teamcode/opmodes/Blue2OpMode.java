c10b15051bcdd844e043b11c262404825ab5f630
Devildogs11206 <devildogs11206@gmail.com>
2021-02-10 16:18:41 -0600

    changed target a

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Blue2OpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Blue2OpMode.java
@@ -32,6 +32,7 @@ public class Blue2OpMode extends BlueOpMode {
         robot.drive(1,0,30,64);
         robot.drive(-1,0,30,5);
         robot.drive(0,1,30,24);
+        robot.drive(1,0, 0);
     }
 
     protected void targetB() {
