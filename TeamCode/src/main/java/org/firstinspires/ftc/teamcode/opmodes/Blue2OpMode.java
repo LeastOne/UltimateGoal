b1c0be314a3f83e061b512a5b06d7daa7ead8195
Devildogs11206 <devildogs11206@gmail.com>
2021-02-10 17:18:14 -0600

    Switched some of the shooter code around

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Blue2OpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Blue2OpMode.java
@@ -32,11 +32,11 @@ public class Blue2OpMode extends BlueOpMode {
 
     private void shootPowerShots() {
         robot.shooter(ON);
-        robot.drive(1,0,10,0); //robot turns to (guessed) orientation to shoot power shot target
+        robot.drive(1,0,-10,0); //robot turns to (guessed) orientation to shoot power shot target
         robot.shooter(SHOOT);
-        robot.drive(1,0,15,0);
+        robot.drive(1,0,-15,0);
         robot.shooter(SHOOT);
-        robot.drive(1,0,20,0);
+        robot.drive(1,0,-20,0);
         robot.shooter(SHOOT);
         robot.drive(1,0,0,8); //make sure to get back on line
     }
