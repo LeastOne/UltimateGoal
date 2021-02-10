be34a6645c6709d2dcbfe7d1dfee27dcbb8aed38
Devildogs11206 <devildogs11206@gmail.com>
2021-02-10 17:12:40 -0600

    Worked on target B autonomous code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Blue2OpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Blue2OpMode.java
@@ -59,7 +59,7 @@ public class Blue2OpMode extends BlueOpMode {
         robot.drive(0,1,0,35);
         robot.drive(1,0,0,42);
         robot.drive(1,0,9,40);
-        robot.drive(-1,0,9,15);
+        robot.drive(-1,0,9,17);
         robot.drive(0,1,0,1);
 
     }
