c8df7ed5860417542e58045ac50a44434a5873c0
Devildogs11206 <devildogs11206@gmail.com>
2021-02-15 15:49:56 -0600

    Added delay after turning wheel on, and turned robot slightly more to the left

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Blue2OpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Blue2OpMode.java
@@ -64,11 +64,12 @@ public class Blue2OpMode extends BlueOpMode {
 
     private void shootPowerShots() {
         robot.shooter(ON);
-        robot.drive(1,0,-10,0); //robot turns to (guessed) orientation to shoot power shot target
+        sleep(1000);
+        robot.drive(1,0,-5,0); //robot turns to (guessed) orientation to shoot power shot target
+        robot.shooter(SHOOT);
+        robot.drive(1,0,-10,0);
         robot.shooter(SHOOT);
         robot.drive(1,0,-15,0);
-        robot.shooter(SHOOT);
-        robot.drive(1,0,-20,0);
         robot.shooter(SHOOT);
         robot.drive(1,0,0,8); //make sure to get back on line
     }
