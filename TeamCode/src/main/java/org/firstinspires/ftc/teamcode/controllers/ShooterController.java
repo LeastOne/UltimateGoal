cb9d20b5990cacb4430e8560f64007d733b8e7ec
Devildogs11206 <devildogs11206@gmail.com>
2021-02-17 16:20:56 -0600

    Added code that lets the robot drivers shoot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/ShooterController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/ShooterController.java
@@ -2,6 +2,10 @@ package org.firstinspires.ftc.teamcode.controllers;
 
 import org.firstinspires.ftc.teamcode.opmodes.OpMode;
 
+import static org.firstinspires.ftc.teamcode.internal.Robot.ShooterMode.OFF;
+import static org.firstinspires.ftc.teamcode.internal.Robot.ShooterMode.ON;
+import static org.firstinspires.ftc.teamcode.internal.Robot.ShooterMode.SHOOT;
+
 public class ShooterController extends RobotController {
     public ShooterController(OpMode opMode) {
         super(opMode);
@@ -9,6 +13,9 @@ public class ShooterController extends RobotController {
 
     @Override
     public void execute() {
+        if (gamepad2.x) robot.shooter(ON);
+        else if (gamepad2.y) robot.shooter(SHOOT);
+        else if (gamepad2.b) robot.shooter(OFF);
 
     }
 }
